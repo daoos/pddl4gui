4527a0f7b34cea9f73ad6bbbadd7efe1531f3249
Emmanuel Hermellin <emmanuel.hermellin@univ-grenoble-alpes.fr>
2018-02-13 10:49:05 +0100

    Add comments in Main and Token (others class to come)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 import com.pagosoft.plaf.PlafOptions;
 import solver.gui.Solver;
 
+/**
+ * This class implements the main method of <code>PDDL4GUI</code>.
+ *
+ * @author E. Hermellin
+ * @version 1.0 - 12.02.2018
+ */
 public class Main {
+
+    /**
+     * The main method of <code>PDDL4GUI</code>.
+     *
+     * @param args the arguments of the command line.
+     */
     public static void main(String[] args) {
         PlafOptions.setAsLookAndFeel();
         PlafOptions.updateAllUIs();
